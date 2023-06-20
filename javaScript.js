c183368087d949b7a9bb91712898571fecdb1399
AsadKhalilRao <mrao.bee18seecs@seecs.edu.pk>
2023-06-20 09:22:53 +0500

    mango

---
+++ javaScript.js
@@ -0,0 +1,10 @@
+// window.addEventListener('resize', function() {
+//     var screenWidth = window.innerWidth;
+//     var textElement = document.getElementById('order_text_div');
+  
+//     // Set the width of the text element based on the screen width
+//     textElement.style.width = screenWidth * 0.4 + 'px';
+//   });
+  
+//   // Initial call to set the initial width based on the screen size
+//   window.dispatchEvent(new Event('resize'));
